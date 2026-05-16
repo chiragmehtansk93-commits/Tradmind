dfce03a92ae1374c44a351b4fb4f807b60ff60c3
chiragmehtansk93-commits <chirag.mehta.nsk93@gmail.com>
2026-05-16 15:19:15 +0530

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <main style={{
+      minHeight: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      background: "#0f172a",
+      color: "white",
+      fontSize: "32px",
+      fontWeight: "bold"
+    }}>
+      TradMind 🚀
+    </main>
+  );
+}
