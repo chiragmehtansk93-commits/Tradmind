233514319b447dafc08929a2e32e645a70684629
chiragmehtansk93-commits <chirag.mehta.nsk93@gmail.com>
2026-05-17 12:20:26 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,16 +1,18 @@
 export default function Home() {
   return (
-    <main style={{
-      minHeight: "100vh",
-      background: "#111827",
-      color: "white",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      flexDirection: "column"
-    }}>
+    <main
+      style={{
+        minHeight: "100vh",
+        background: "#111827",
+        color: "white",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
       <h1>TradMind 🚀</h1>
-      <p>Website working successfully</p>
+      <p>TradMind Live</p>
     </main>
   );
 }
